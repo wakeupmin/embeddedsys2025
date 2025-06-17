1beaed94d5408ff63aabea43c0aecc1afbb11304
wakeupmin <kartricky@naver.com>
2025-06-17 16:16:30 +0900

    ..

--- integration/led.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LED_H_
-#define _LED_H_
-
-int ledLibInit(void);
-int ledOnOff (int ledNum, int onOff);
-int ledStatus (void);
-int ledLibExit(void);
-#define LED_DRIVER_NAME "/dev/periled"
-#endif
