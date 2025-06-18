207d863d6f8e5e3cd13ad4f7ede244ef70829095
wakeupmin <kartricky@naver.com>
2025-06-18 15:53:09 +0900

    .

---
+++ week_12/fnd.c
@@ -0,0 +1,69 @@
+#include <fcntl.h>
+#define ONE_SEG_DISPLAY_TIME_USEC   1000
+#define FND_DRIVER_NAME "/dev/perifnd"
+#define MODE_STATIC_DIS 0
+#include "fnd.h"
+
+
+int fndDisp(int num , int dotflag)
+{
+   int fd;
+   int temp,i;
+   stFndWriteForm stWriteData;
+   
+   for (i = 0; i < MAX_FND_NUM ; i++ )
+   {
+      stWriteData.DataDot[i] = (dotflag & (0x1 << i)) ? 1 : 0;  
+      stWriteData.DataValid[i] = 1;
+   }
+   // if 6 fnd
+   temp = num % 1000000;   stWriteData.DataNumeric[0]= temp /100000;
+   temp = num % 100000;   stWriteData.DataNumeric[1]= temp /10000;
+   temp = num % 10000;      stWriteData.DataNumeric[2] = temp /1000;
+   temp = num %1000;      stWriteData.DataNumeric[3] = temp /100;
+   temp = num %100;      stWriteData.DataNumeric[4] = temp /10;
+                     stWriteData.DataNumeric[5] = num %10;
+
+   fd = open(FND_DRIVER_NAME,O_RDWR);
+   if ( fd < 0 )
+   {
+      perror("driver open error.\n");
+      return 0;
+   }   
+   write(fd,&stWriteData,sizeof(stFndWriteForm));
+   close(fd);
+   return 1;
+}
+
+
+
+int fndInit(void)
+{
+    int fd;
+   fd = open(FND_DRIVER_NAME,O_RDWR);
+   if ( fd < 0 )
+   {
+      perror("driver open error.\n");
+      return 0;
+   }   
+}
+
+
+int fndExit(void)
+{
+    int fd;
+    int i;
+
+    stFndWriteForm stWriteData;
+   
+    for (i = 0; i < MAX_FND_NUM ; i++ )
+    {
+        stWriteData.DataDot[i] =  0;  
+        stWriteData.DataNumeric[i] = 0;
+        stWriteData.DataValid[i] = 0;
+    }
+    write(fd,&stWriteData,sizeof(stFndWriteForm));
+    close(fd);
+    return 1;
+
+}
---
+++ week_12/textlcd.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "textlcd.h"
+#define TEXTLCD_DRIVER_NAME "/dev/peritextlcd"
+
+int text(const char *str1 , const char *str2)
+{
+	unsigned int linenum = 0;
+	stTextLCD stlcd;
+	int fd;
+	int len;
+
+	memset(&stlcd,0,sizeof(stTextLCD));
+
+	stlcd.cmdData = CMD_DATA_WRITE_LINE_1;
+	len =strlen(str1);
+
+	if ( len > COLUMN_NUM)
+		memcpy(stlcd.TextData[stlcd.cmdData - 1], str1, COLUMN_NUM);
+	else
+		memcpy(stlcd.TextData[stlcd.cmdData - 1], str1, len);
+
+	stlcd.cmd = CMD_WRITE_STRING;
+	fd = open(TEXTLCD_DRIVER_NAME, O_RDWR);
+
+	if ( fd<0) {
+		perror("driver (//dev//peritxtlcd)) open error.\n");
+		return -1;
+	}
+
+	write(fd,&stlcd,sizeof(stTextLCD));
+
+	stlcd.cmdData = CMD_DATA_WRITE_LINE_2;
+	len = strlen(str2);
+
+	if(len > COLUMN_NUM)
+		memcpy(stlcd.TextData[stlcd.cmdData - 1], str2, COLUMN_NUM);
+	else
+		memcpy(stlcd.TextData[stlcd.cmdData -1 ], str2, len);
+
+	stlcd.cmd = CMD_WRITE_STRING;
+
+	write(fd, &stlcd, sizeof(stTextLCD));
+
+	close(fd);
+
+	return 0;
+
+}
